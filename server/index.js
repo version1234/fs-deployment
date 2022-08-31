c433f41984fba34e1354b020e9d627494a459520
suneetha kanumurit <suneetaraj@gmail.com>
2022-08-31 14:45:32 -0500

    recommit

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'));
+    res.sendFile(path.join(__dirname, '../webPage.html'));
 });
 
 const port = process.env.PORT || 4005;
